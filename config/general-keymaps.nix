2a1e800284d1f5d4e09d3b0ca0dec21b4f51bd8a
Keryx <florian.koenig@smake.com>
2026-04-22 08:24:44 +0200

    feat: close all floating windows (hover popups, etc.) on ESC

--- config/general-keymaps.nix
+++ config/general-keymaps.nix
@@ -1,15 +1,18 @@
 {
   # General editor keybindings (non-LSP, non-plugin specific)
-  keymaps = [
-    # Clear search highlighting when pressing Esc
-    {
-      mode = "n";
-      key = "<Esc>";
-      action = "<cmd>nohlsearch<cr>";
-      options = {
-        desc = "Clear search highlighting";
-        silent = true;
-      };
-    }
-  ];
+
+  extraConfigLua = ''
+    -- Clear search highlighting and close all floating windows on ESC
+    vim.keymap.set("n", "<Esc>", function()
+      vim.cmd("nohlsearch")
+      
+      -- Close any floating windows (hover popups, diagnostics, etc.)
+      for _, win in ipairs(vim.api.nvim_list_wins()) do
+        local config = vim.api.nvim_win_get_config(win)
+        if config.relative ~= "" then  -- floating window
+          vim.api.nvim_win_close(win, false)
+        end
+      end
+    end, { silent = true, noremap = true, desc = "Clear search highlighting and close floating windows" })
+  '';
 }
