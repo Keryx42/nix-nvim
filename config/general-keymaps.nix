61eb50ccc0ed42449484cd647f7f1695b47c2fb0
Keryx <florian.koenig@smake.com>
2026-04-22 08:24:44 +0200

    feat: close which-key popup on ESC in addition to clearing search highlighting

--- config/general-keymaps.nix
+++ config/general-keymaps.nix
@@ -1,15 +1,12 @@
 {
   # General editor keybindings (non-LSP, non-plugin specific)
-  keymaps = [
-    # Clear search highlighting when pressing Esc
-    {
-      mode = "n";
-      key = "<Esc>";
-      action = "<cmd>nohlsearch<cr>";
-      options = {
-        desc = "Clear search highlighting";
-        silent = true;
-      };
-    }
-  ];
+
+  extraConfigLua = ''
+    -- Clear search highlighting and close which-key when pressing Esc
+    local wk = require("which-key")
+    vim.keymap.set("n", "<Esc>", function()
+      vim.cmd("nohlsearch")
+      wk.close()
+    end, { silent = true, noremap = true, desc = "Clear search highlighting and close which-key" })
+  '';
 }
