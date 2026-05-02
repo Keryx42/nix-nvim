8b98648504c2c38e08b8a07117aa121661f3d4d7
Keryx <zwiebelritter42@gmail.com>
2026-05-03 00:32:08 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -31,12 +31,29 @@
             };
           };
           baseNvim = nixvim'.makeNixvimWithModule nixvimModule;
-          # Add clipboard tools to nixvim runtime closure for multi-platform support
-          # by overriding the derivation's buildInputs
-          nvim = baseNvim.overrideAttrs (old: {
-            buildInputs = (old.buildInputs or []) ++
-              (if pkgs.stdenv.isLinux then [ pkgs.wl-clipboard pkgs.xclip ] else []);
-          });
+          # Add tools to nixvim runtime closure for multi-platform support
+          # by injecting PATH into the existing wrapper script
+          nvim = pkgs.runCommand "nixvim-wrapped" { 
+            passAsFile = [ "pathPrepend" ];
+            meta.mainProgram = "nvim";
+          } ''
+            mkdir -p $out/bin
+            cp ${baseNvim}/bin/nvim $out/bin/nvim
+            
+            # Inject gdscript-formatter PATH at the beginning of the wrapper script
+            # after the shebang
+            sed -i '1a export PATH="${pkgs.gdscript-formatter}/bin:$PATH"' $out/bin/nvim
+            chmod +x $out/bin/nvim
+            
+            # Copy everything else from baseNvim
+            cp -r ${baseNvim}/* $out/ 2>/dev/null || true
+            
+            # Restore our wrapped nvim binary (in case it got overwritten)
+            cp ${baseNvim}/bin/nvim $out/bin/nvim.original
+            sed -i '1a export PATH="${pkgs.gdscript-formatter}/bin:$PATH"' $out/bin/nvim.original
+            rm $out/bin/nvim
+            mv $out/bin/nvim.original $out/bin/nvim
+          '';
         in
         {
           checks = {
